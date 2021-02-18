1e29b0fa27f20a2232067a30444225d6ba753b97
Jeroen Claassens <support@favware.tech>
2021-02-18 20:38:27 +0100

    chore: i don't believe it

--- scripts/rollup.build.ts
+++ scripts/rollup.build.ts
@@ -3,7 +3,7 @@ import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
-	input: './src/index.ts',
+	input: 'src/index.ts',
 	output: [
 		{
 			file: './dist/index.js',
@@ -25,7 +25,7 @@ export default {
 		}
 	],
 	plugins: [
-		typescript({ tsconfig: resolveDir(__dirname, '..', 'src', 'tsconfig.json') }),
+		typescript({ tsconfig: resolveDir(process.cwd(), 'src', 'tsconfig.json') }),
 		terser({
 			ecma: 2019,
 			// This will ensure that whenever Rollup is in watch (dev) mode, console logs will not be removed
