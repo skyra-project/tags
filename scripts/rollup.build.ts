de1438acfbc18f4d4bd512025ea290ac608d59f9
Jeroen Claassens <support@favware.tech>
2021-02-18 20:36:22 +0100

    fix: umd bundle name and type bundling

--- scripts/rollup.build.ts
+++ scripts/rollup.build.ts
@@ -1,10 +1,9 @@
 import { resolve as resolveDir } from 'path';
-import cleaner from 'rollup-plugin-cleaner';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
-	input: 'src/index.ts',
+	input: './src/index.ts',
 	output: [
 		{
 			file: './dist/index.js',
@@ -21,15 +20,12 @@ export default {
 		{
 			file: './dist/index.umd.js',
 			format: 'umd',
-			name: 'SkyraChar',
+			name: 'SkyraTags',
 			sourcemap: true
 		}
 	],
 	plugins: [
-		cleaner({
-			targets: ['./dist/']
-		}),
-		typescript({ tsconfig: resolveDir(__dirname, 'src', 'tsconfig.json') }),
+		typescript({ tsconfig: resolveDir(__dirname, '..', 'src', 'tsconfig.json') }),
 		terser({
 			ecma: 2019,
 			// This will ensure that whenever Rollup is in watch (dev) mode, console logs will not be removed
